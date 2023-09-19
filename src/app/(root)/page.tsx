6a15b4594238e60e218e7bba639bac0abfeadcd8
Suresh Krishna <krishnapaulraj2004@gmail.com>
2023-09-19 21:53:50 +0530

    Auth Update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Helllo testing
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      Helllo testing
     </main>
   );
 }
--- src/app/api/sign-up/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  try {
-    const { name, email, password } = await request.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
